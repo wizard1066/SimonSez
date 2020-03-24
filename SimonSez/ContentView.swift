ebbda8abb55124ad5e0417ebf72945ab3825fe72
Mark Lucking <mark.lucking@gmail.com>
2020-03-24 08:59:46 +0100

    Initial Commit

---
+++ SimonSez/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimonSez
+//
+//  Created by localadmin on 24.03.20.
+//  Copyright © 2020 Mark Lucking. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
